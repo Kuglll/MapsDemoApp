5406e74c2c7e71d044a173b3665d1bd76c7bbff1
Kuglll <mark.frelih@gmail.com>
2023-11-13 20:48:14 +0100

    Setup Hilt

---
+++ app/src/main/java/com/example/mapsdemoapp/MapsDemoApplication.kt
@@ -0,0 +1,7 @@
+package com.example.mapsdemoapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MapsDemoApplication: Application()
--- app/src/main/java/com/example/mapsdemoapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/mapsdemoapp/ui/MainActivity.kt
@@ -1,10 +1,12 @@
-package com.example.mapsdemoapp
+package com.example.mapsdemoapp.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.mapsdemoapp.ui.map.MapScreen
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
